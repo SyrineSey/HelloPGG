89d949cdd1de9b45b3b3a73c4daa3d0719c70bc3
Syrine IHADDADEN <syrine.ihaddaden@innovia-dz.com>
2019-04-01 12:18:02 +0200

    init

---
+++ app.py
@@ -0,0 +1,18 @@
+def multiply(*numbers):
+    total = 1
+    for number in numbers:
+        total *= number
+    return total
+
+
+print("start")
+print(multiply(1, 2, 3))
+
+
+# f9:  add break point
+# f5: run the application app to this point
+# f10: execute one statement at the time
+# f11: jump into the function
+
+
+# dupplicate line or multiple lines: press shift alt and down key
